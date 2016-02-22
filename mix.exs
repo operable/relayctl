17dfe08105dd29555ed5ad9966fc833382f78a05
Kevin Smith <kevin@operable.io>
2016-02-22 16:51:57 -0500

    bumping vsn

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Relayctl.Mixfile do
 
   def project do
     [app: :relayctl,
-     version: "0.0.1",
+     version: "0.2.0",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
