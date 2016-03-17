7ca9e8dfe3a66b98efbf7784a097cd850fe6bb4a
Kevin Smith <kevin@operable.io>
2016-03-17 18:19:44 -0400

    Bumped version

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Relayctl.Mixfile do
 
   def project do
     [app: :relayctl,
-     version: "0.2.0",
+     version: "0.3.0",
      elixir: "~> 1.2",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
